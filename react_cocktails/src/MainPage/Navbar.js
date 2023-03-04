97bb1a4078e1dcb85e96cef544db26732a8735f9
druckhead <daniel32raz@gmail.com>
2023-03-04 23:12:56 +0200

    fix bug where text would show up over navbar

--- react_cocktails/src/MainPage/Navbar.js
+++ react_cocktails/src/MainPage/Navbar.js
@@ -6,23 +6,21 @@ import Typography from "@mui/material/Typography";
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ position: "sticky", top: 0 }}>
-      <AppBar position="static" sx={{ height: 100 + "%" }}>
-        <Toolbar>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{
-              display: "flex",
-              flexGrow: 1,
-              justifyContent: "center",
-              fontSize: 1.6 + "em",
-            }}
-          >
-            Cocktails
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="sticky" sx={{ top: 0 }}>
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{
+            display: "flex",
+            flexGrow: 1,
+            justifyContent: "center",
+            fontSize: 1.6 + "em",
+          }}
+        >
+          Cocktails
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
