dfe431d213d4f713cd10eb818b4328bd6a2e4790
druckhead <daniel32raz@gmail.com>
2023-03-04 23:22:55 +0200

    minimize font size for cocktailitem

--- react_cocktails/src/MainPage/CockTailItem/Instructions.js
+++ react_cocktails/src/MainPage/CockTailItem/Instructions.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Instructions(props) {
   return (
-    <Container sx={{ m: 0, p: 0, pr: 2 + "em", fontSize: 1.5 + "em" }}>
+    <Container sx={{ m: 0, p: 0, pr: 2 + "em", fontSize: 1.2 + "em" }}>
       <ol>{props.instructions}</ol>
     </Container>
   );
