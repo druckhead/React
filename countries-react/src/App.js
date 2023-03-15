f4883e183a532b88618c8431561f4f2c6f2de962
druckhead <daniel32raz@gmail.com>
2023-03-15 10:43:33 +0200

    .

--- countries-react/src/App.js
+++ countries-react/src/App.js
@@ -1,25 +1,36 @@
-import './App.css';
-import Layout from './Layout/Layout';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Home/Home';
-import CountriesPage from './Countries/CountriesPage';
-import CountryDetails from './Countries/CountryDetails';
-import CountdownPage from './Countdown/CountdownPage';
-
+import "./App.css";
+import Layout from "./Layout/Layout";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home/Home";
+import CountriesPage from "./Countries/CountriesPage";
+import CountryDetails from "./Countries/CountryDetails";
+import CountdownPage from "./Countdown/CountdownPage";
+import { useState } from "react";
 
 function App() {
+  const [secondsInput, setSecondsInput] = useState("");
+  const [countdownStarted, setCountdownStarted] = useState(false);
   return (
-
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="countries/" element={<CountriesPage />}>
-              <Route path=":countryId/" element={ <CountryDetails />}/>
-          </Route>
-          <Route path="countdown/" element={<CountdownPage />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="countries/" element={<CountriesPage />}>
+          <Route path=":countryId/" element={<CountryDetails />} />
         </Route>
-      </Routes>
-  )
+        <Route
+          path="countdown/"
+          element={
+            <CountdownPage
+              secondsInput={secondsInput}
+              setSecondsInput={setSecondsInput}
+              countdownStarted={countdownStarted}
+              setCountdownStarted={setCountdownStarted}
+            />
+          }
+        />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
